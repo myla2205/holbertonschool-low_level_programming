974a4013b724fc74b83a7cfb7aa96bfd4f189812
Kamyl <myla2205@gmail.com>
2024-06-18 11:39:55 -0400

    numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * Print numbers 0 to 9
- *
- * Return- Always (Success)
- int main(void)
-
- {
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
 	int n;
 
-	for (n = '0'; <= n = '9'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		puchar(n)
-		}
+		putchar(n);
+	}
 
-		putchar('/n');
+	putchar('\n');
 
-		return (0);
-		}
+	return (0);
+}
