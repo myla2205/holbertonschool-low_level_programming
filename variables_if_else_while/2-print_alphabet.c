11722ce26e66e2e60d248f03b025839ca2787b24
Kamyl <myla2205@gmail.com>
2024-06-18 10:39:26 -0400

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+int main(void)
+{
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        putchar(letter);
+    }
+    putchar('\n');  // Print a newline character after printing the alphabet
+
+    return (0);
+}
