6728d2993a7c8cc2d5cb69391353eb00bd39a8dd
Kamyl <myla2205@gmail.com>
2024-06-18 13:28:35 -0400

    combination

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Print combination of number
+ *
+ * Return- Always (Succes)
+ */
+int main(void)
+{
+	int num1,num2;
+
+	for (num2 = 0; num 1 <= 9; num 1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			printf ("%d%d", num1, num2);
+		}
+	}
+
+	printf ("\n");
+
+	return (0);
+}
