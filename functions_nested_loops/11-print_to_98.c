67a76cec6926cc623ec6c1ec0b93746477dbbcc2
Kamyl <myla2205@gmail.com>
2024-06-19 14:13:50 -0400

    add task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,42 +1,23 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * times_table - Prints the 9 times table, starting with 0
- * Return: empty output
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
  */
-
-void times_table(void)
-
+void print_to_98(int n)
 {
-	int x, y, z, u, d;
-
-	for (x = 0; y <= 9; x++)
+	if (n >= 98)
 	{
-		for (y = 0; y <= 9; y++)
-		{
-			z = x * y;
-			if (z > 9)
-			{
-				u = z % 10;
-				d = (z - u) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
-			}
-			else
-			{
-				if (y != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
-				_putchar(z + '0');
-			}
-		}
-		_putchar('\n');
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
-
