c829aab29f6565eee9c764a3b30b3e4e9fdd625a
Kamyl <myla2205@gmail.com>
2024-06-19 09:16:42 -0400

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main- Print Putchar
+ *
+ *Description: This function prints "_putchar" followed by a newline
+ * character to standard output using the write system call
+ *
+ * Return- Always (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Functions
+Nested
+Loops
