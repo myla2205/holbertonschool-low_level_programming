3b814fb89738f0fc9c1b23886391194b9824c293
Kamyl <myla2205@gmail.com>
2024-06-19 10:14:17 -0400

    asdm

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  *Description: This function prints "_putchar" followed by a newline
  * character to standard output using the write system call
  *
- * Return- Always (Success)
+ * Return: Always (Success)
  */
 int main(void)
 {
 	char str[] = "_putchar";
 	int n = 0;
-		for(n = 0; n < 8; n++)
+		for (n = 0; n < 8; n++)
 		{
 			_putchar(str[n]);
 		}
