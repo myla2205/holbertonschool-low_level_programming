4505a47e07bd56382b7f4233fd27127f66525f29
Kamyl <myla2205@gmail.com>
2024-06-24 11:17:41 -0400

    update

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * Take a pointer to an int as a parameter
+ * Update the value it points to to 98
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
