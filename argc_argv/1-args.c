cc0eabd550d8770971e352e8c994ab6cbc0f31df
Kamyl <myla2205@gmail.com>
2024-07-02 10:39:32 -0400

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code for Holberton School students.
+ * @argc: The size of the argv array.
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: Always argc.
+ */
+int main(int argc, char **argv)
+{
+	if (*argv[0] > 0)
+		printf("%i\n", argc - 1);
+	return (0);
+}
