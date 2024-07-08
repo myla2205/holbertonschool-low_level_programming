de8ffd0d8dadc2802330ee65434cdf1c377e7e0c
Kamyl <myla2205@gmail.com>
2024-07-08 13:06:14 -0400

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef Dog
+#define Dog
+/**
+ * struct dog - description of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
