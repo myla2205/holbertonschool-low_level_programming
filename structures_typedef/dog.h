d215962af1eba65c7eb87f5ecb6e05ddeae77f4e
Kamyl <myla2205@gmail.com>
2024-07-09 08:55:08 -0400

    init

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,35 @@
-#ifndef Dog
-#define Dog
-/**
- * struct dog - description of the dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
+#ifndef DOG_H
+#define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+
+/**
+*dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
 #endif
