fa221e3365b5a428c76971d539c7dbcb3504c207
Kamyl <myla2205@gmail.com>
2024-07-14 17:32:45 -0400

    print

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,12 @@
 #include "function_pointers.h"
 
 /**
-  * print_name - Prints a name
-  * @name: The name to prints
-  * @f: Pointer to function
-  *
-  * Return: Nothing
-  */
+ * print_name - Print a name from a function pointer
+ * @name: char string
+ * @f: function pointer that takes a string argument
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	f(name);
 }
