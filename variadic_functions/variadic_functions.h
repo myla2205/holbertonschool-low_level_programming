2a545099133f3d93ba9c3ab6f3eed1a40bbefc16
Kamyl <myla2205@gmail.com>
2024-07-14 18:27:17 -0400

    print

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,11 @@
+#include <stdio.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list ap);
+void print_integer(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
+
