7ae193eb93a5ada25add90225f1801fb6e5be0bc
Kamyl <myla2205@gmail.com>
2024-07-24 11:13:10 -0400

    elements

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,20 +1,15 @@
 #include "lists.h"
 
 /**
- * list_len - the number of elements in a linked list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
-
+  * list_len - finds the number of nodes in a list
+  * @h: singly linked list
+  * Return: number of elements in a linked list
+  */
 size_t list_len(const list_t *h)
 {
+	int i;
 
-	size_t i;
-
-	i = 0;
-
-		while (h != NULL)
-			h = h->next;
-	i++;
+	for (i = 0; h; i++)
+		h = h->next;
 	return (i);
 }
