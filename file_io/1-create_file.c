10e04b975470ec3862292626bb6e7d40e6afee6c
Kamyl <myla2205@gmail.com>
2024-07-29 15:14:23 -0400

    create

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,25 +1,28 @@
 #include "main.h"
 /**
- * create_file - creates a file.
- * @filename: The file's name.
- * @text_content: The file's content.
- * Return: 1 on sucess or -1 on failure.
- */
+  * create_file - creates a file with rw------- permissions
+  * @filename: name of the file, if NULL, return -1
+  * @text_content: contents of the file. If NULL, create an empty file
+  * Return: 1 on success, -1 on failure
+  */
 int create_file(const char *filename, char *text_content)
 {
-	int fdesc, error, size = 0;
+	int new_file, len, wr_stat;
 
-	if (filename)
+	if (filename == NULL)
+		return (-1);
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (new_file == -1)
+		return (-1);
+	if (text_content == NULL)
 	{
-		fdesc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0500);
-		if (fdesc > 0)
-		{
-			while (text_content && text_content[size] != '\0')
-				size++;
-			error = write(fdesc, text_content, size);
-			close(fdesc);
-			return ((error >= 0) ? 1 : -1);
-		}
+		close(new_file);
+		return (1);
 	}
-	return (-1);
+	for (len = 0; text_content[len]; len++)
+		;
+	wr_stat = write(new_file, text_content, len);
+	if (close(new_file) == -1)
+		return (-1);
+	return (wr_stat == -1 ? -1 : 1);
 }
