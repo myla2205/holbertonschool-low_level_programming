9ad53562629b2734895b09cc2f9fb700ccd960a0
Kamyl <myla2205@gmail.com>
2024-07-29 15:11:11 -0400

    create

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,25 +1,38 @@
 #include "main.h"
+
 /**
- * create_file - creates a file.
- * @filename: The file's name.
- * @text_content: The file's content.
- * Return: 1 on sucess or -1 on failure.
+ * create_file - creates of a file.
+ * @filename: filename
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fdesc, error, size = 0;
+	int fd;
+	int nletters;
+	int mnm;
 
-	if (filename)
-	{
-		fdesc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-		if (fdesc > 0)
-		{
-			while (text_content && text_content[size] != '\0')
-				size++;
-			error = write(fdesc, text_content, size);
-			close(fdesc);
-			return ((error >= 0) ? 1 : -1);
-		}
-	}
-	return (-1);
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0500);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	mnm = write(fd, text_content, nletters);
+
+	if (mnm == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
